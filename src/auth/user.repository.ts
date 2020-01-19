e14342eea480917a59bf91b7cd9bc2c5d9541ecf
Prithwiraj Sinha <prithwiraj.sinha.dev@gmail.com>
2020-01-18 22:08:28 -0600

    implement username and password check

---
+++ src/auth/user.repository.ts
@@ -0,0 +1,47 @@
+import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
+import { User } from "./user.entity";
+import { Repository, EntityRepository } from "typeorm";
+import {
+  ConflictException,
+  InternalServerErrorException
+} from "@nestjs/common";
+
+import * as bcrypt from "bcrypt";
+
+@EntityRepository(User)
+export class UserRepository extends Repository<User> {
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+
+    const user = new User();
+    user.username = username;
+    user.salt = await bcrypt.genSalt();
+    user.password = await this.hashPassword(password, user.salt);
+    try {
+      await user.save();
+    } catch (error) {
+      if (error.code === "23505") {
+        //duplicate user name
+        // TODO: can keep a file for having all error codes in one place
+        throw new ConflictException("username already exists");
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
+  }
+
+  async validateUserPassword(authCredentialsDto: AuthCredentialsDto) {
+    const { username, password } = authCredentialsDto;
+    const user = await this.findOne({ username });
+
+    if (user && (await user.validatePassword(password))) {
+      return user.username;
+    } else {
+      return null;
+    }
+  }
+
+  private async hashPassword(password: string, salt: string) {
+    return bcrypt.hash(password, salt);
+  }
+}
